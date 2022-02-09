5437df00d2e2931f43673d6bf8a100571dd9188f
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-02-09 16:15:51 +0300

    Added Routes for my page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { Repository } from './repository';
+import { RepositoryComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home-page', component: HomePageComponent},
+  {path:'Repository', component:RepositoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
