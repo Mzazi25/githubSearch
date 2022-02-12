7c9d50ea7d48f3dbd080135309ab73d2d2f9741a
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-02-12 12:27:17 +0300

    Added redirect on 404 errors to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { RepositoryComponent } from './repository/repository.component';
 
 const routes: Routes = [
   {path:'home-page', component: HomePageComponent},
-  {path:'Repository', component:RepositoryComponent}
+  {path:'Repository', component:RepositoryComponent},
+  { path:'**', component:NotFoundComponent},
+  { path: '', redirectTo:"/home-page", pathMatch:"full"},
 ];
 
 @NgModule({
